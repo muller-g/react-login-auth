85a88aed5432277106379915c8838a37a93b4162
gabrielmuller <gabriel_muller98@hotmail.com>
2022-03-03 17:34:21 -0300

    att

--- src/template/pages/Login.js
+++ src/template/pages/Login.js
@@ -8,37 +8,35 @@ import openEye from '../../img/eye.png'
 import closedEye from '../../img/closedeye.png'
 
 function Login(){
-    const { authenticated, login } = useContext(AuthContext);
+    const { login } = useContext(AuthContext);
     const [eyeImg, setEyeImg] = useState(openEye);
 
-    function handdleClick(){
-        if(eyeImg == openEye){
+    function handdleClick(){    
+         if(eyeImg === openEye){
             setEyeImg(closedEye);
             document.querySelector('input:nth-child(4)').type = "";
         } else {
             setEyeImg(openEye)
             document.querySelector('input:nth-child(4)').type = "password";
-        }
-    }
+        } 
+    } 
 
     function handleSubmit(e){
         e.preventDefault();
-        const data  = {
+        axios.post('https://communicationadmin.grupostra.com/auth/local', {
             identifier: user,
             password: pass
-        }
-        axios.get('https://communicationadmin.grupostra.com/api/auth/local', {
-            identifier: 'teste',
-            password: 'teste123'
         })
-        .then(response => { console.log(response)
-             //if(response.data.jwt != "" && response.data.user.confirmed == true){
-                //login(user, pass);
-            // } 
+        .then(response => { 
+             if(response.data.jwt != "" && response.data.user.confirmed === true){
+                login(user, pass);
+            } 
          }) .catch(error => 
                 alert("Usuário ou senha incorretos")
-            )    
+        )    
+
     }
+
     const [user, setUser] = useState('');
     const [pass, setPass] = useState('');
 
@@ -47,15 +45,15 @@ function Login(){
             <div className={styles.form_container}>
                 <img src={logo} alt="Logo Grupo" srcset="" />
                 <p>Bem Vindo(a), entre com seu usuário e senha.</p>
-                    <form onSubmit={handleSubmit}>
-                        <label htmlFor="username">Usuário</label>
-                        <Input type="text" name="username" id="username" placeholder="Usuário" setProps={setUser} value={user}/>
-                        <label htmlFor="password">Senha</label>
-                        <Input type="password" name="password" id="password" placeholder="Senha" setProps={setPass} value={pass}/>
-                        <span onClick={handdleClick}><img src={eyeImg} className={styles.eyeImg} /></span>
-                        <Input className="btn-hover" type="submit" name="submit" id="submit" value="Entrar" />
-                    </form>
-                </div>
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor="username">Usuário</label>
+                    <Input type="text" name="username" id="username" placeholder="Usuário" setProps={setUser} value={user}/>
+                    <label htmlFor="password">Senha</label>
+                    <Input type="password" name="password" id="password" placeholder="Senha" setProps={setPass} value={pass}/>
+                    <span onClick={handdleClick}><img src={eyeImg} className={styles.eyeImg} /></span>
+                    <Input className="btn-hover" type="submit" name="submit" id="submit" value="Entrar" />
+                </form>
+            </div>
         </div>
     )
 }
